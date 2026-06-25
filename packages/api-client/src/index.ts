42c79db40356c3ea816fb24b7b54467c61718297
MKS~ZAK <60817481+mks-zakaria@users.noreply.github.com>
2026-06-25 04:45:42 +0100

    feat(irrigation): port irrigation-automation farmer feature from agri-front

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -7,6 +7,7 @@ export * from './alertApi';
 export * from './api';
 export * from './checkAuthTokens';
 export * from './clearSsoSession';
+export * from './irrigationAutomationApi';
 export * from './index';
 export * from './loginApiError';
 export * from './logoutEverywhere';
