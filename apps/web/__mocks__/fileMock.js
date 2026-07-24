37bd3e18e69867d00aa83231f3b77b7b50adf418
MKS~ZAK <60817481+mks-zakaria@users.noreply.github.com>
2026-07-24 09:40:07 +0100

    refactor(login): migrate the login form from antd to Chakra (#115)

---
+++ apps/web/__mocks__/fileMock.js
@@ -0,0 +1,3 @@
+// Static asset stub for jest. Next imports images (png/jpg/svg/…) as an
+// object describing the asset; under jest we only need the import to resolve.
+module.exports = { src: '/logo.png', height: 44, width: 44 };
